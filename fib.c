46efd69b0e0b1d0edcc13c3a61d57e7468e06d61
xMashu <crazytnt22@gmail.com>
2024-12-02 12:03:16 -0800

    revised code

--- fib.c
+++ fib.c
@@ -1,97 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int iterative(int n)
+int iterative(int combinedNthValue)
 {
-   if (n == 0)
+   if (combinedNthValue == 0)
    {
       return 0;
    }
-   else if(n == 1)
+   else if (combinedNthValue == 1)
    {
       return 1;
    }
-
    int current = 1;
    int prev = 0;
    int next = 0;
-   for(int i = 1; i < n; ++i)
+   for (int i = 1; i < combinedNthValue; ++i)
    {
       next = prev + current;
       prev = current;
       current = next;
    }
    return current;
-
 }
 
-int recursion(int n)
+int recursion(int combinedNthValue)
 {
-   if(n == 0)
+   if (combinedNthValue == 0)
    {
       return 0;
    }
-   else if (n == 1)
+   else if (combinedNthValue == 1)
    {
       return 1;
    }
    else
    {
-      return recursion(n-1) + recursion(n-2);
+      return recursion(combinedNthValue - 1) + recursion(combinedNthValue - 2);
+   }
+}
+// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Iterative-Fibonacci.html
+//  helped with understanding iterative fib again
+
+int main(int argc, char *argv[])
+{
+
+   if (argc < 4)
+   {
+      printf("Too many arguments, only need int, r/i, and filename\n");
+      exit(EXIT_FAILURE);
    }
 
-
-}
-
-//https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Iterative-Fibonacci.html
-// helped with understanding iterative fib again
-
-
-
-
-
-int main(int argc, char *argv[]) {
-
-   int  commandLineInt = 0;
+   int commandLineInt = 0;
    sscanf(argv[1], "%d", &commandLineInt);
-   //https://www.geeksforgeeks.org/c-program-for-char-to-int-conversion/
+   // https://www.geeksforgeeks.org/c-program-for-char-to-int-conversion/
 
    char fibOption = *argv[2];
    char *filename = argv[3];
 
-
-   //scanf("%s", filename);
-   FILE *file = fopen(filename, "r"); 
+   // scanf("%s", filename);
+   FILE *file = fopen(filename, "r");
    // https://www.tutorialspoint.com/c_standard_library/c_function_fopen.htm
-
 
    int fileInteger;
    fscanf(file, "%d", &fileInteger);
-   //https://www.tutorialspoint.com/c_standard_library/c_function_fscanf.htm
+   // https://www.tutorialspoint.com/c_standard_library/c_function_fscanf.htm
    fclose(file);
 
-   int n = 0;
-   n = commandLineInt + fileInteger -1; 
+   int combinedNthValue = 0;
+   combinedNthValue = commandLineInt + fileInteger - 1;
    // -1 cuz fib has to start at 0
 
-
-
-
    int result = 0;
- 
-   if(fibOption == 'i')
+
+   if (fibOption == 'i')
    {
-      result = iterative(n);
+      result = iterative(combinedNthValue);
    }
-   else if(fibOption == 'r')
+   else if (fibOption == 'r')
    {
-      result = recursion(n);
+      result = recursion(combinedNthValue);
    }
 
-   printf("%d", result);
-
-
-
-
-
+   printf("%d\n", result);
 }
